69060a8a6a94d18390ab8aaf307a160069c1c383
Matthew Heyerly <matthew.heyerly@gmail.com>
2025-11-14 19:38:32 +0000

    input output exercises

---
+++ input_output_exercises/age.py
@@ -0,0 +1,7 @@
+current_age = int(input('how old are you? '))
+
+print(f'you are {current_age} years old')
+print(f'in 10 years, you will be {current_age + 10}')
+print(f'in 20 years, you will be {current_age + 20}')
+print(f'in 30 years, you will be {current_age + 30}')
+print(f'in 40 years, you will be {current_age + 40}')
---
+++ input_output_exercises/greeter.py
@@ -0,0 +1,12 @@
+print('what is your first name? ')
+
+first_name = input()
+
+print('what is your last name? ')
+
+last_name = input()
+
+print(f'hello, {first_name} {last_name}!')
+
+
+
